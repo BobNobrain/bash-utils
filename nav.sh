f431d7f8e4e755f5639ace50ef3bb2b42a3cdb48
BobNobrain <nikita-ivanov.mail@yandex.ru>
2017-02-27 22:45:28 +0300

    Some additions to nav

--- nav.sh
+++ nav.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# up
 up () {
     if [ "${1}." == "--help." -o "${1}." == "-h." ]; then
         echo 'Usage: '
@@ -45,5 +46,30 @@ up () {
 
 alias back='cd "$OLDPWD"'
 
+# Stay & ret
 alias stay='SAVEDPWD="$PWD"'
 alias ret='if [ "${SAVEDPWD}." == . ]; then echo "No saved path"; else cd "$SAVEDPWD"; fi'
+
+# utilies for working with dirs
+alias md=mkdir
+alias mkpath="mkdir -p"
+mdc () {
+    mkdir $*
+
+    path="$1"
+    while ! [ -d "$path" ]; do
+        shift
+        path="$1"
+    done
+
+    if [ -d "$path" ]; then
+        cd "$path"
+    fi
+}
+
+alias rmd="rm -r"
+rmup () {
+    path="$PWD"
+    cd ..
+    rm -r "$path"
+}
